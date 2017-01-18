8064acf617ddeacbaed69ab2e63158280b3e8a32
Stu Peters <stu_peters187@hotmail.com>
2017-01-18 09:52:13 -0500

    build addPerson function

--- contracts/People.sol
+++ contracts/People.sol
@@ -8,5 +8,17 @@ contract People {
     uint age;
   }
 
+  function addPerson(string _firstName, string _lastName, uint _age) returns (bool success) {
+
+    Person memory newPerson;
+    newPerson.firstName = _firstName;
+    newPerson.lastName = _lastName;
+    newPerson.age = _age;
+
+    people.push(newPerson);
+    return true;
+
+  }
+
 
 }
